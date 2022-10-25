a4ed3b6d480878dd2a0393c39d6a24288cf59579
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2022-10-25 06:02:14 +0600

    courses prams id added

--- index.js
+++ index.js
@@ -16,6 +16,15 @@ app.get("/courses", (req, res) => {
   res.send(courses);
 });
 
+app.get('/courses/:id', (req, res) => {
+
+  const id = parseInt(req.params.id);
+  const course = courses.find(course => course.id === id);
+  res.send(course);
+
+})
+
+
 app.listen(port, () => {
   console.log(`StudyCation server is running on port ${port}`);
 });
